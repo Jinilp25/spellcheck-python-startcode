01dbda499d641268d1bc186d18b73d4e2a4c2ea4
Jinilp25 <112644667+Jinilp25@users.noreply.github.com>
2023-02-22 13:46:09 -0700

    Update spellcheck.py

--- spellcheck.py
+++ spellcheck.py
@@ -6,6 +6,27 @@
 import re  # Needed for splitting text with a regular expression
 
 
+def linearSearch(anList, item):
+    for n in anList:
+        if n == item:
+            return anList.index(n)
+    return -1
+
+
+def binarySearch(anList, item):
+    li = 0
+    ui = len(anList) - 1
+    while ui >= li:
+        mi = (li + ui) // 2
+        if item == anList[mi]:
+            return mi
+        elif item < anList[mi]:
+            ui = mi - 1
+        else:
+            li = mi + 1
+    return -1
+
+
 def main():
     # Load data files into lists
     dictionary = loadWordsFromFile("data-files/dictionary.txt")
@@ -14,7 +35,29 @@ def main():
     # Print first 50 values of each list to verify contents
     print(dictionary[0:50])
     print(aliceWords[0:50])
+
+    while True:
+        print("\nMain Menu")
+        print("1: Spell Check a Word (Linear Search)")
+        print("2: Spell Check a Word (Binary Search)")
+        print("3: Spell Check Alice In Wonderland (Linear Search)")
+        print("4: Spell Check Alice in Wonderland (Binary Search)")
+        print("5: Exit")
+        selection = input("Enter menu selection (1-5): ")
+        word_type = input("Please enter a word: ")
+
+        if selection == "1":
+            x = linearSearch(dictionary, word_type)
+            print(x)
+        elif selection == "2":
+            y = binarySearch(aliceWords, word_type)
+            print(y)
+        elif selection == "3":
+            w = linearSearch(aliceWords, word_type)
+            print(w)
 # end main()
+
+# "python.terminal.executeInFileDir": true in preference settings from file checkmark
 
 
 def loadWordsFromFile(fileName):
@@ -30,13 +73,3 @@ def loadWordsFromFile(fileName):
 
 # Call main() to begin program
 main()
-
-while True:
-    print("Main Menu")
-    print("1: Spell Check a Word (Linear Search)")
-    print("2: Spell Check Alice In Wonderland (Linear Search)")
-    print("3: Spell Check Alice In Wonderland (Linear Search)")
-    print("4: Spell Check Alice in Wonderland (Binary Search)")
-    print("5: Exit")
-    selection = input("Enter menu selection (1-5): ")
-    input("")
